90acefdd6a1f273642adfdee791772302f800393
Allan Denot <adenot@gmail.com>
2019-08-17 18:11:26 +1000

    Optinal creation of route53 record

--- _variables.tf
+++ _variables.tf
@@ -3,3 +3,8 @@ variable "s3_bucket_id" {}
 variable "hostname" {}
 variable "hosted_zone" {}
 variable "certificate_arn" {}
+
+variable "hostname_create" {
+  description = "Create hostname in the hosted zone passed?"
+  default     = true
+}

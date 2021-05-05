5ae4592852011cd8979ca0b3fa95705e1e2ce35d
Allan Denot <adenot@gmail.com>
2021-05-05 17:19:53 +1000

    Adding policy to force SSL

--- s3-policy.tf
+++ s3-policy.tf
@@ -20,6 +20,22 @@ data "aws_iam_policy_document" "s3_policy" {
       identifiers = [aws_cloudfront_origin_access_identity.default[0].iam_arn]
     }
   }
+
+  statement {
+    sid    = "ForceSSLOnlyAccess"
+    effect = "Deny"
+    actions = ["s3:*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    resources = [data.aws_s3_bucket.selected[0].arn, "${data.aws_s3_bucket.selected[0].arn}/*"]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = [false]
+    }
+  }
 }
 
 resource "aws_s3_bucket_policy" "oai" {

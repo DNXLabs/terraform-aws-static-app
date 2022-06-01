a80cc12069218e0684885452cc5920ea8333dc5c
Lucas Gothelipe <lucasqg@outlook.com>
2022-06-01 06:51:09 +0000

    lint

--- _settings.tf
+++ _settings.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.7.0"
-      configuration_aliases = [ aws.us-east-1 ]
+      source                = "hashicorp/aws"
+      version               = ">= 2.7.0"
+      configuration_aliases = [aws.us-east-1]
     }
   }
 }

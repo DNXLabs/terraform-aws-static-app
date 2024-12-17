61dd337dd4000c317dfbdf62e681994eb317e28f
Allan Denot <adenot@gmail.com>
2024-12-17 14:46:03 +1000

    Add output for key groups (#20)

--- _outputs.tf
+++ _outputs.tf
@@ -7,3 +7,16 @@ output "cloudfront_distribution_id" {
   description = "CloudFront Distribution ID"
   value       = var.module_enabled ? aws_cloudfront_distribution.default[0].id : null
 }
+
+output "cloudfront_key_group_id" {
+  description = "CloudFront Key Group ID"
+  value = {
+    for k, keygroup in aws_cloudfront_key_group.default : k => keygroup.id
+  }
+}
+output "cloudfront_public_key_id" {
+  description = "CloudFront Key Group ID"
+  value = {
+    for k, publickey in aws_cloudfront_public_key.default : k => publickey.id
+  }
+}
